1f538b9050fb56a578c7394f85cbb67f9d467a88
SimonGer <gersimonger@gmail.com>
2024-10-25 12:54:13 +0200

    Updated Carla urls

--- setup_carla.sh
+++ setup_carla.sh
@@ -3,10 +3,10 @@
 # Download and install CARLA
 mkdir carla
 cd carla
-wget https://carla-releases.s3.eu-west-3.amazonaws.com/Linux/CARLA_0.9.10.1.tar.gz
-wget https://carla-releases.s3.eu-west-3.amazonaws.com/Linux/AdditionalMaps_0.9.10.1.tar.gz
-tar -xf CARLA_0.9.10.1.tar.gz
-tar -xf AdditionalMaps_0.9.10.1.tar.gz
-rm CARLA_0.9.10.1.tar.gz
-rm AdditionalMaps_0.9.10.1.tar.gz
+wget https://tiny.carla.org/carla-0-9-10-linux
+wget https://tiny.carla.org/additional-maps-0-9-10-linux
+tar -xf carla-0-9-10-linux
+tar -xf additional-maps-0-9-10-linux
+rm carla-0-9-10-linux
+rm additional-maps-0-9-10-linux
 cd ..
